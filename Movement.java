f2041e751c1a979518a1cfe01e2b6513b20807bc
NecroTheif <Alwaygetgo@gmail.com>
2014-11-15 17:55:49 -0500

    Added Behavior Movement. Ability to move to other actors or follow a path

---
+++ Movement.java
@@ -0,0 +1,161 @@
+import greenfoot.*;
+import java.util.List;
+
+/**
+ * The Beahvior of an Entity to Move
+ * 
+ * @author NecroTheif 
+ * @version 2014.13.11
+ */
+public class Movement extends Behavior {
+
+    private Actor actor; // An actor for the target of movement
+    
+    
+    /**
+     * Initilizes a Movement Behavior for the given entity
+     * 
+     * @param entity   The entity that has this behavior
+     */
+    public Movement(Entity entity){
+        
+        /* Call the superclass' constructor to store the given entity */
+            super(entity);
+        
+    }// End one-argument constructor for Movement
+    
+    
+    /**
+     * Moves the Entity to the closest part of the path and 
+     * moves it along the path until it reaches the end.
+     * 
+     * @return   If the Entity has reached the end of the path.
+     */
+    public boolean moveAlongPath(Path path){
+        
+        /* 
+         * Intilize a varible to hold the road piece at the entity's postion and set it to 
+         * the road piece at the entity's position if there is one or set it to null if 
+         * there isn't 
+         */
+            RoadPiece roadPiece = entity.getObjectsInRange(0, RoadPiece.class).size()!=0 ?
+                                    (RoadPiece) entity.getObjectsInRange(0, RoadPiece.class).get(0) :
+                                    null;
+            
+                                    
+        /* Check to see if the Entity is heading toward the path, following the path, or neither */
+            if(step==-1){// Entity has not started yet
+                
+                /* Find the closest road piece */
+                    roadPiece = (RoadPiece) entity.getClosestActor(RoadPiece.class);
+                            
+                            
+                /* Turn towards the closest road piece */
+                    entity.turnTowards(roadPiece.getX(), roadPiece.getY());
+                    
+                    
+                /* Set step to value where Entity is moving toward the path */
+                    step = -2;
+                
+            }
+            else if(step==-2){// Entity is heading toward the path
+                
+                /* Get the roadpieces the entity is on */
+                    List<RoadPiece> roadPieces = entity.getIntersectingObjects(RoadPiece.class);
+                
+                    
+                /* Check if the entity is on the path yet and if on the center */
+                    if(roadPiece!=null){
+                        
+                        /* Set the step variable to the path step the entity is at */
+                            step = path.getRoadPieceNum(roadPiece);
+                            
+                    }// End if(path.isRoadAt(entity.getX(), entity.getY()))
+                    else if(roadPieces.size()!=0){
+                        
+                        /* Move closer to the center of the square */
+                            entity.turnTowards(
+                                roadPieces.get(0).getX(), roadPieces.get(0).getY());
+                            entity.move(1);
+                        
+                    }// End else if(entity.getIntersectingObjects(RoadPiece.class).size()!=0)
+                    else{
+                        
+                        /* move towards the closest road piece */
+                            entity.move(1);
+                        
+                    }// End else for if(path.isRoadAt(entity.getX(), entity.getY()))
+                
+            }// End if(step==-1)
+            else{// Entity is following the path
+                    
+                /* Find the next road piece */
+                    roadPiece = path.getRoadPiece(step+1);
+                    
+                    
+                /* Check to make sure there is a next road piece */
+                    if(roadPiece!=null){
+                        
+                        /* Check if the entity is on the next path step yet */
+                            if(roadPiece.getX()==entity.getX() &&
+                                roadPiece.getY()==entity.getY()){
+                                
+                                /* Incriment the step variable to the next path step */
+                                    step++;
+                                    
+                            }// End if(roadPiece==path.getRoadPiece(step+1))
+                            else{
+                                
+                                /* Turn and move towards the next road piece */
+                                    entity.turnTowards(roadPiece.getX(), roadPiece.getY());
+                                    entity.move(1);
+                                
+                            }// End else for (roadPiece.getX()==entity.getX()...
+                        
+                    }// End if(roadPiece!=null)
+               
+            }// End else for if(step==-1)
+            
+            
+        /* Return if the entity has reached the end of the path yet or not */
+            return roadPiece!=null && path.getLastRoadPiece()==roadPiece;
+        
+    }// End method moveAlongPath
+    
+    
+    /**
+     * Moves the Entity to the closest actor of a given class
+     * 
+     * @param cls      The class of the actor to move to
+     * @param offset   The offset the Entity can be to have "reached" the actor
+     * @return         If the Entity has reached one yet with the given offset
+     */
+    public boolean moveToClosest(Class cls, int offset){
+        
+        /* Check to see if the ENtity is move towards the Actor yet or not */
+            if(step==-1){
+                
+                /* Get and store the closest Actor of the given class */
+                    actor = entity.getClosestActor(cls);
+                
+                /* Set the step variable to 0 to indicate starting movement */
+                    step = 0;
+                
+            }// End if(step==-1)
+                
+            
+        /* Turn and move towards the actor */
+            entity.turnTowards(actor.getX(), actor.getY());
+            entity.move(1);
+            
+            
+        /* Return If the Entity has reached that actor with the given offset */
+            return entity.getX()<=actor.getX()+offset &&
+                        entity.getX()>=actor.getX()-offset &&
+                        entity.getY()<=actor.getY()+offset &&
+                        entity.getY()>=actor.getY()-offset;
+        
+    }// End method moveToClosest
+    
+    
+}// End class Movement
