a1d45a95025d2b02ad72a6d5f073cca28a03881f
NecroTheif <Alwaygetgo@gmail.com>
2014-11-15 17:57:12 -0500

    Added Actors class - Class to hold methods that all my actors with have. Added Level class - class that holds the current level (world)

---
+++ Actors.java
@@ -0,0 +1,115 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+import java.awt.Point;
+
+/**
+ * Added Methods to Actor class
+ * 
+ * @author NecroTheif 
+ * @version 2014.14.11
+ */
+public class Actors extends Actor{
+    
+    /**
+     * Return all the objects that intersect this object. This takes the graphical extent of objects into consideration. 
+     * 
+     * @param cls   Class of objects to look for (passing 'null' will find all objects).
+     * @return      all the objects that intersect this object
+     */
+    @Override
+    public List getIntersectingObjects(java.lang.Class cls){
+        
+        /* Call the superclass' protected method with given values */
+            return super.getIntersectingObjects(cls);
+        
+    }// End method getIntersectingObjects
+    
+    
+    /**
+     * Return all objects within range 'radius' around this object. An object is within 
+     * range if the distance between its centre and this object's centre is less than or 
+     * equal to 'radius'.
+     * 
+     * @param radius   Radius of the circle (in cells)
+     * @param cls      Class of objects to look for (passing 'null' will find all objects)
+     * @return         all objects within range 'radius' around this object
+     */
+    @Override
+    public List getObjectsInRange(int radius, java.lang.Class cls){
+        
+        /* Call the superclass' protected method with given values */
+            return super.getObjectsInRange(radius, cls);
+        
+    }// End method getObjectsInRange
+        
+    
+    /**
+     * Gets the closest actor of a given class to this actor
+     * 
+     * @param cls   The class of the actors to look for
+     */
+    public Actor getClosestActor(Class cls){
+        
+        /* Get all objects in this actor's world */
+            List<Actor> actors = getWorld().getObjects(cls);
+            
+            
+        /* Intilize Point variable to hold this actor's postion for easy distance calculations */
+            Point postion = new Point(getX(), getY());
+            
+        /* 
+         * Intilize variable to hold the most current Actor that is the closest and
+         * set it equal to the first actor only if the first actor is not this one and
+         * if it is set it equal to the second actor
+         */
+            Actor closestActor = actors.get(0)==(Actor)this ? actors.get(1) : actors.get(0);
+            
+            
+        /* 
+         * Intilize a variable to hold the currently closest distance from the closest 
+         * actor to this one and set it equal to the distance between this and closestActor
+         */
+            double closestDistance = postion.distance(closestActor.getX(), closestActor.getY());
+        
+            
+        /* Check all of the actors postions */
+            for(int i=0;i<actors.size();i++){
+                
+                /* Check to make sure the current actor is not this actor */
+                    if(actors.get(i)!=(Actor)this){
+                        
+                       /* Get the distance from current actor to this actor */
+                           double currentDistance = 
+                                    postion.distance(
+                                        actors.get(i).getX(), actors.get(i).getY());
+                                        
+                                        
+                       /* Check to see if the current distance is smaller than the closest */
+                           if(currentDistance<closestDistance){
+                               
+                               /* Set the new closest variable's to this actor's */
+                                   closestDistance = currentDistance;
+                                   closestActor = actors.get(i);
+                               
+                            }// End if(currentDistance<closestDistance)
+                       
+                    }// End if(actors.get(i)!=(Actor)this)
+                
+            }// End for(int i=0;i<actors.size();i++)
+        
+            
+        /* Return the closestActor */
+            return closestActor;
+            
+    }// End method getCLosestObject(Class object)
+    
+    
+    /**
+     * Act - do whatever the Actors wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ Level.java
@@ -0,0 +1,76 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.Color;
+
+/**
+ * A Test level of USCB Tower Defense
+ * 
+ * @author NecroTheif
+ * @version 2014.14.14
+ */
+public class Level extends World{
+
+    private Path path; // The current Path of the Level
+    private USCB uscb; // The current USCB Bulding of the Level
+    
+    
+    /**
+     * Create A Level with road shape "Z"
+     */
+    public Level()
+    {    
+        
+        /* 
+         * Create a new world with 600x400 cells with a cell size of 1x1 pixels 
+         * and green background. 
+         */
+            super(600, 400, 1); 
+            setBackground(new GreenfootImage("green-grass-texture.jpg"));
+            
+        /* Create Road */
+            path = new Path(25, PathShape.HORIZONTAL_BOLT, 0, 50, 550, 50);
+            path.createPath();
+            path.addToWorld(this);
+            
+            
+        /* Create the Bulding */
+            uscb = new USCB(100, 50);
+            addObject(uscb, 550, 50);
+            
+            
+        /* Create And Place Random TestEntities */
+            for(int i=0;i<15;i++){
+                
+                TestEntity testEntity = new TestEntity();
+                addObject(testEntity, Greenfoot.getRandomNumber(601), Greenfoot.getRandomNumber(401));
+                
+            }// End for(int i=0;i<15;i++)
+            
+    }// End zero-argument consturctor for USCB
+    
+    
+    /**
+     * Gets the current Path of the Level
+     * 
+     * @return   The current Path of the Level
+     */
+    public Path getPath(){
+        
+        /* Return the current Path */
+            return path;
+        
+    }// End method getPath
+    
+    
+    /**
+     * Gets the current USCB Bulding of the Level
+     * 
+     * @return   The current USCB Bulding of the Level
+     */
+    public USCB getUSCB(){
+        
+        /* Return the current USCB */
+            return uscb;
+            
+    }// End method USCB
+    
+}// End class USCB
